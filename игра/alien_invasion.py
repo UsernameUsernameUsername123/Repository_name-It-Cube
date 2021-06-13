e973b1140d2f3b12cb761019b308b759fe7a27bf
UsernameUsernameUsername123 <mywork_2021@mail.ru>
2021-06-13 15:45:01 +0300

    add game

---
+++ игра/alien_invasion.py
@@ -0,0 +1,41 @@
+import pygame
+from pygame.sprite import Group
+
+from settings import Settings
+from game_stats import GameStats
+from ship import Ship
+import game_functions as gf
+
+
+def run_game():
+    # Инициализирует pygame, settings и обьект экрана.
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+
+    stats = GameStats(ai_settings)
+
+    bg_color = (230, 230, 230)
+
+    # Создание корабля.
+    ship = Ship(ai_settings, screen)
+    bullets = Group()
+    aliens = Group()
+
+    gf.create_fleet(ai_settings, screen, ship, aliens)
+
+    # запуск основного цикла игры.
+    while True:
+        gf.check_events(ai_settings, screen, ship, bullets)
+
+        if stats.game_active:
+            ship.update()
+            gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
+            gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
+
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
+
+
+run_game()
